cb8842388b4c4c47025a62eb12c59d96820aa877
Rikardo <rikardo@marenzzi.de>
2020-09-25 11:32:29 +0200

    Password Validierung

--- src/main/java/de/neuefische/cologne/passwordexercise/PasswordCheck.java
+++ src/main/java/de/neuefische/cologne/passwordexercise/PasswordCheck.java
@@ -2,4 +2,68 @@ package de.neuefische.cologne.passwordexercise;
 
 public class PasswordCheck {
 
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static String checkPassword(String password) {
+       //return checkPasswordLength(password) && checkPasswordNumberIncluded(password) && checkPasswordLowercaseIncluded(password) && checkPasswordUppercaseIncluded(password);
+
+        if (!checkPasswordLength(password)) {
+            return "Passwort muss mind. 8 Zeichen enthalten";
+        }
+        if (checkPasswordNumberIncluded(password) == false) {
+            return "Passwort muss mind. eine Zahl enthalten";
+        }
+        if (checkPasswordLowercaseIncluded(password) == false) {
+            return "Passwort muss mind. einen kleinen Buchsaben enthalten";
+        }
+        if (checkPasswordUppercaseIncluded(password) == false) {
+            return "Passwort muss mind. einen großen Buchstaben enthalten";
+        }
+
+        return "Passwort ist GEIL";
+
+
+    }
+
+
+    public static boolean checkPasswordLength(String password) {
+
+        return password.length() >= 8;
+
+    }
+
+    public static boolean checkPasswordNumberIncluded (String password) {
+
+        if (password.matches(".*\\d.*")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static boolean checkPasswordLowercaseIncluded(String password) {
+
+        if (password.matches(".*\\p{Lower}.*")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static boolean checkPasswordUppercaseIncluded(String password) {
+
+        if (password.matches(".*\\p{Upper}.*")) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
 }
--- src/test/java/de/neuefische/cologne/passwordexercise/PasswordCheckTest.java
+++ src/test/java/de/neuefische/cologne/passwordexercise/PasswordCheckTest.java
@@ -1,7 +1,127 @@
 package de.neuefische.cologne.passwordexercise;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordCheckTest {
+
+    //Check Password Length
+
+    @Test
+    @DisplayName("Das Password muss mind. 8 Zeichen lang sein")
+    public void testCheckPasswordLengthTrue() {
+        //GIVEN
+        String inputPassword = "aaaaaaaa";
+        //WHEN
+        Boolean resultLength = PasswordCheck.checkPasswordLength(inputPassword);
+        //THEN
+        assertEquals(true, resultLength);
+    }
+
+    @Test
+    @DisplayName("Das Password muss mind. 8 Zeichen lang sein")
+    public void testCheckPasswordLengthFalse() {
+        //GIVEN
+        String inputPassword = "aaaaaaa";
+        //WHEN
+        Boolean result= PasswordCheck.checkPasswordLength(inputPassword);
+        //THEN
+        assertEquals(false, result);
+    }
+
+    //Check Number Included
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 Zahl enthalten")
+    public void testCheckPasswordNumberIncludedTrue() {
+        //GIVEN
+        String inputPassword = "aaaaaaa97895675asdaf";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordNumberIncluded(inputPassword);
+        //THEN
+        assertEquals(true, result);
+    }
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 Zahl enthalten")
+    public void testCheckPasswordNumberIncludedFalse() {
+        //GIVEN
+        String inputPassword = "aaaaaaaa";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordNumberIncluded(inputPassword);
+        //THEN
+        assertEquals(false, result);
+    }
+
+    //Check if Lowercase Included
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 kleinen Buchstaben enthalten")
+    public void testCheckPasswordLowercaseIncludedTrue() {
+        //GIVEN
+        String inputPassword = "aAAAAAAA";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordLowercaseIncluded(inputPassword);
+        //THEN
+        assertEquals(true, result);
+    }
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 kleinen Buchstaben enthalten")
+    public void testCheckPasswordLowercaseIncludedFalse() {
+        //GIVEN
+        String inputPassword = "AAAAAAAA";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordLowercaseIncluded(inputPassword);
+        //THEN
+        assertEquals(false, result);
+    }
+
+    //Check if Lowercase Included
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 großen Buchstaben enthalten")
+    public void testCheckPasswordUppercaseIncludedTrue() {
+        //GIVEN
+        String inputPassword = "Aaaaaaaa";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordUppercaseIncluded(inputPassword);
+        //THEN
+        assertEquals(true, result);
+    }
+
+    @Test
+    @DisplayName("Das Password muss mind. 1 großen Buchstaben enthalten")
+    public void testCheckPasswordUppercaseIncludedFalse() {
+        //GIVEN
+        String inputPassword = "aaaaaaaaa";
+        //WHEN
+        Boolean result = PasswordCheck.checkPasswordUppercaseIncluded(inputPassword);
+        //THEN
+        assertEquals(false, result);
+    }
+
+    //
+
+    @ParameterizedTest (name="InputPassword {0} returnString {1}")
+    @CsvSource({
+            "aaaaaaa, Passwort muss mind. 8 Zeichen enthalten",
+            "aaaaaaaa, Passwort muss mind. eine Zahl enthalten",
+            "AAAAAAA1, Passwort muss mind. einen kleinen Buchsaben enthalten",
+            "aa2abas2, Passwort muss mind. einen großen Buchstaben enthalten",
+            "a1Fhsw5t, Passwort ist GEIL"
+    })
+    public void testCheckPassword(String password, String expectedString){
+        String result = PasswordCheck.checkPassword(password);
+        assertEquals(expectedString,result);
+    }
+
+
+
+
 
 }
